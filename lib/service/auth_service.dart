e1070b49cb874e14a7a4e2d5507ad7de80c22f50
Ashiqur Rahman <ashiqur17sep@gmail.com>
2023-02-12 10:08:13 +0600

    second commit

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,31 +1,52 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
 import 'package:chatapp_firebase/service/database_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class AuthService {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
-  //login
+  // login
+  Future loginWithUserNameandPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password))
+          .user!;
 
-  //register
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  // register
   Future registerUserWithEmailandPassword(
       String fullName, String email, String password) async {
     try {
       User user = (await firebaseAuth.createUserWithEmailAndPassword(
-              email: email, password: password))
+          email: email, password: password))
           .user!;
 
       if (user != null) {
-        //call our database service to update the user data
-        DatabaseService(uid: user.uid).updateUserData(fullName, email);
-
+        // call our database service to update the user data.
+        await DatabaseService(uid: user.uid).savingUserData(fullName, email);
         return true;
       }
-    } on FirebaseAuthException catch (ex) {
-      print(ex);
-      return ex;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
     }
   }
 
-  //signout
+  // signout
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,59 +1,41 @@
-import 'package:chatapp_firebase/shared/constast.dart';
 import 'package:flutter/material.dart';
 
-//InputDecoration for TextField
 const textInputDecoration = InputDecoration(
   labelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w300),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-      color: Color(0xFFee7b64),
-      width: 2,
-    ),
+    borderSide: BorderSide(color: Color(0xFFee7b64), width: 2),
   ),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-      color: Color(0xFFee7b64),
-      width: 2,
-    ),
+    borderSide: BorderSide(color: Color(0xFFee7b64), width: 2),
   ),
   errorBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-      color: Color(0xFFee7b64),
-      width: 2,
-    ),
+    borderSide: BorderSide(color: Color(0xFFee7b64), width: 2),
   ),
 );
-// Page Route
+
 void nextScreen(context, page) {
-  Navigator.of(context).push(MaterialPageRoute(builder: (_) => page));
+  Navigator.push(context, MaterialPageRoute(builder: (context) => page));
 }
 
 void nextScreenReplace(context, page) {
-  Navigator.of(context)
-      .pushReplacement(MaterialPageRoute(builder: (_) => page));
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => page));
 }
 
-//Snackbar
 void showSnackbar(context, color, message) {
-
   ScaffoldMessenger.of(context).showSnackBar(
-
-     SnackBar(
+    SnackBar(
       content: Text(
         message,
-        style: TextStyle(
-          fontSize: 14,
-        ),     
-      ),
-      duration: const Duration(seconds: 5),
-      action: SnackBarAction(
-        onPressed: (){
-
-        },
-        label:"Ok",
-        textColor: Colors.white,
+        style: const TextStyle(fontSize: 14),
       ),
       backgroundColor: color,
+      duration: const Duration(seconds: 2),
+      action: SnackBarAction(
+        label: "OK",
+        onPressed: () {},
+        textColor: Colors.white,
       ),
+    ),
   );
 }
